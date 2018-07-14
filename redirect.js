c937714c08df6e8acc988aaad956b0da471b9028
mlaci <marko.laszlo.laci@gmail.com>
2018-07-14 17:43:58 +0200

    redirect to last commit

--- redirect.js
+++ redirect.js
@@ -1,6 +1,6 @@
 if(/https:\/\/mlaci.github.io\/vb/.test(location.href)){
   var res = /https?:\/\/mlaci.github.io\/vb\/(#[0-9]+p)/.exec(location.href)
   var quality = res && res[1] || ""
-  fetch("https://gitcdn.xyz/repo/mlaci/vb/master/index.html")
-  .then(res=>location.href = "http" + res.url.slice(5) + quality)
+  fetch("https://api.github.com/repos/mlaci/vb/commits").then(res=>res.json())
+  .then(([last])=>location.href = "http://gitcdn.xyz/cdn/mlaci/vb/" + last.sha + "/index.html" + quality)
 }
